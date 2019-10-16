3f896f9c71d90114ffa18e7b71d173795acd4301
Radek Forgač <radek.forgac@applifting.cz>
2019-10-16 23:06:40 +0200

    init

---
+++ config/fish/aliases.fish
@@ -0,0 +1,23 @@
+alias tf 'terraform'
+
+alias d 'docker'
+alias dc 'docker-compose'
+alias dm 'docker-machine'
+
+alias k 'kubectl'
+
+alias rm 'trash'
+alias cat 'bat'
+
+alias ls 'exa'
+alias l 'ls'
+alias ll 'ls -lah'
+
+alias pbp 'pbpaste'
+alias pbc 'pbcopy'
+
+alias cd.. 'cd ..'
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../..'
